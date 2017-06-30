01e6ee89d3a0e49d3d27a836cff49a8620f30869
BoBRoID <n.gilko@gmail.com>
2017-07-01 01:02:51 +0300

    some edited

--- console/config/main.php
+++ console/config/main.php
@@ -26,6 +26,9 @@ return [
                 ],
             ],
         ],
+        'request'	=>	[
+            'class'	=>	'console\components\Request'
+        ],
     ],
     'params' => $params,
 ];
