da62fbaef3b34a5e02130b753d37b507657b5a6c
BoBRoID <n.gilko@gmail.com>
2017-07-01 01:10:41 +0300

    some improvments

--- environments/dev/tg/config/params-local.php
+++ environments/dev/tg/config/params-local.php
@@ -1,8 +1,5 @@
 <?php
 return [
-    'siteName'      =>  '',
-    'apiKey'        =>  '',
-    'botName'       =>  'MuzOneBot',
     'botanKey'      =>  '',
     'adminsId'      =>  [],
     'maxFileSize'   =>  (10 * 1024 * 1024) - 2
