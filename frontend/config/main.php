e5d333268c6c61294a7a25f278d508ac7a4f0036
BoBRoID <n.gilko@gmail.com>
2017-07-01 18:25:54 +0300

    add some debug

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -44,7 +44,7 @@ return [
         ],
         'session' => [
             // this is the name of the session cookie used for login on the frontend
-            'name' => 'advanced-frontend',
+            'name' => 'muzone-frontend',
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
