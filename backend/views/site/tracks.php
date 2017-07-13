dda3139dc11c840b04331e6603696c2233f44e46
BoBRoID <n.gilko@gmail.com>
2017-07-14 00:27:54 +0300

    hotfix

--- backend/views/site/tracks.php
+++ backend/views/site/tracks.php
@@ -53,123 +53,133 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <h1><?=$this->title?></h1>
 <hr>
-<div class="well">
-    Фильтры
-    <?php $form = \yii\bootstrap\ActiveForm::begin(['method' => 'get', 'layout' => 'horizontal']); ?>
-    <?=$form->field($searchModel, 'query')?>
-    <?=$form->field($searchModel, 'artist')?>
-    <?=$form->field($searchModel, 'trackName')?>
-    <div class="text-center"><button type="submit" class="btn btn-success"><?=\Yii::t('manage', 'Фильтровать список треков')?></button></div>
-    <?php $form->end(); ?>
+<div class="card">
+    <div class="card-header">
+        <?=\Yii::t('manage', 'Фильтры')?>
+    </div>
+    <div class="card-block">
+        <?php $form = \yii\bootstrap\ActiveForm::begin(['method' => 'get', 'layout' => 'horizontal']); ?>
+        <?=$form->field($searchModel, 'query')?>
+        <?=$form->field($searchModel, 'artist')?>
+        <?=$form->field($searchModel, 'trackName')?>
+        <div class="text-center">
+            <button type="submit" class="btn btn-success"><?=\Yii::t('manage', 'Фильтровать список треков')?></button>
+        </div>
+        <?php $form->end(); ?>
+    </div>
 </div>
-<?=\yii\grid\GridView::widget([
-    'dataProvider'  =>  $dataProvider,
-    'tableOptions'  =>  [
-        'class' =>  'table table-condensed table-striped'
-    ],
-    'pager'     =>  [
-        'options'           =>  [
-            'class' =>  'pagination mx-auto mt-3 d-inline-flex'
-        ],
-        'linkOptions'       =>  [
-            'class'             =>  'page-link',
-        ],
-        'pageCssClass'      =>  'page-item',
-        'prevPageCssClass'  =>  'page-item',
-        'nextPageCssClass'  =>  'page-item',
-        'disabledListItemSubTagOptions' =>  [
-            'class' =>  'page-link'
-        ]
-    ],
-    'columns'       =>  [
-        [
-            'attribute' =>  'id'
-        ],
-        [
-            'label'     =>  \Yii::t('manage', 'Название '),
-            'attribute' =>  'title',
-            'format'    =>  'html',
-            'value'     =>  function($model) use($searchModel){
-                /**
-                 * @var $model \common\models\Song
-                 */
-                return Html::a($model->title, Url::current([
-                    $searchModel->formName() => ['trackName' => $model->title]
-                ]), ['title' => \Yii::t('site', 'Искать треки с названием {name}', ['name' => $model->title])]);
-            }
-        ],
-        [
-            'label'     =>  \Yii::t('manage', 'Исполнитель'),
-            'attribute' =>  'artist',
-            'format'    =>  'html',
-            'value'     =>  function($model) use($searchModel){
-                /**
-                 * @var $model \common\models\Song
-                 */
-                return Html::a($model->artist, Url::current([
-                    $searchModel->formName() => ['artist' => $model->artist]
-                ]), ['title' => \Yii::t('site', 'Искать треки исполнителя {name}', ['name' => $model->artist])]);
-            }
-        ],
-        [
-            'label'     =>  \Yii::t('manage', 'Длина'),
-            'attribute' =>  'duration',
-            'contentOptions'    =>  [
-                'class' =>  'text-center'
+<div class="card">
+    <div class="card-block">
+        <?=\yii\grid\GridView::widget([
+            'dataProvider'  =>  $dataProvider,
+            'tableOptions'  =>  [
+                'class' =>  'table table-condensed table-striped'
             ],
-            'value'     =>  function($model){
-                $minutes = floor($model->duration / 60);
-                $seconds = $model->duration - ($minutes * 60);
+            'pager'     =>  [
+                'options'           =>  [
+                    'class' =>  'pagination mx-auto mt-3 d-inline-flex'
+                ],
+                'linkOptions'       =>  [
+                    'class'             =>  'page-link',
+                ],
+                'pageCssClass'      =>  'page-item',
+                'prevPageCssClass'  =>  'page-item',
+                'nextPageCssClass'  =>  'page-item',
+                'disabledListItemSubTagOptions' =>  [
+                    'class' =>  'page-link'
+                ]
+            ],
+            'columns'       =>  [
+                [
+                    'attribute' =>  'id'
+                ],
+                [
+                    'label'     =>  \Yii::t('manage', 'Название '),
+                    'attribute' =>  'title',
+                    'format'    =>  'html',
+                    'value'     =>  function($model) use($searchModel){
+                        /**
+                         * @var $model \common\models\Song
+                         */
+                        return Html::a($model->title, Url::current([
+                            $searchModel->formName() => ['trackName' => $model->title]
+                        ]), ['title' => \Yii::t('site', 'Искать треки с названием {name}', ['name' => $model->title])]);
+                    }
+                ],
+                [
+                    'label'     =>  \Yii::t('manage', 'Исполнитель'),
+                    'attribute' =>  'artist',
+                    'format'    =>  'html',
+                    'value'     =>  function($model) use($searchModel){
+                        /**
+                         * @var $model \common\models\Song
+                         */
+                        return Html::a($model->artist, Url::current([
+                            $searchModel->formName() => ['artist' => $model->artist]
+                        ]), ['title' => \Yii::t('site', 'Искать треки исполнителя {name}', ['name' => $model->artist])]);
+                    }
+                ],
+                [
+                    'label'     =>  \Yii::t('manage', 'Длина'),
+                    'attribute' =>  'duration',
+                    'contentOptions'    =>  [
+                        'class' =>  'text-center'
+                    ],
+                    'value'     =>  function($model){
+                        $minutes = floor($model->duration / 60);
+                        $seconds = $model->duration - ($minutes * 60);
 
-                if($seconds <= 9){
-                    $seconds = '0'.$seconds;
-                }
+                        if($seconds <= 9){
+                            $seconds = '0'.$seconds;
+                        }
 
-                return $minutes.':'.$seconds;
-            }
-        ],
-        [
-            'label'     =>  \Yii::t('manage', 'Добавил'),
-            'format'    =>  'html',
-            'attribute' =>  'user_id',
-            'value'     =>  function($model, $value) use ($searchModel){
-                if(!$model->user){
-                    return null;
-                }
-                /**
-                 * @var $model \common\models\Song
-                 */
-                return Html::a($model->user->username ? : $model->user->first_name.' '.$model->user->last_name, Url::current([$searchModel->formName() => ['userId' => $model->user_id]]));
-            }
-        ],
-        [
-            'label'     =>  \Yii::t('manage', 'Добавлено'),
-            'attribute' =>  'added',
-            'format'    =>  'html',
-            'contentOptions'    =>  [
-                'class' =>  'text-center'
-            ],
-            'value'     =>  function($model){
-                return \Yii::$app->formatter->asDatetime($model->added, 'MM/dd/yyyy').'&nbsp;'.Html::tag('br', null, ['class' => 'hidden-md hidden-lg']).\Yii::$app->formatter->asDatetime($model->added, 'HH:mm');
-            }
-        ],
-        [
-            'class'     =>  \yii\grid\ActionColumn::className(),
-            'buttons'   =>  [
-                'edit'  =>  function($url, $model){
-                    return Html::a(FA::i('pencil'), Url::to(['track/edit', 'id' => $model->id]), ['class' => 'btn btn-secondary']);
-                },
-                'play'  =>  function($model){
-                    return Html::button(FA::i('play'), ['class' => 'btn btn-secondary pull-left']);
-                },
-                'download'  =>  function($url, $model){
-                    return Html::a(FA::i('save'), ['/site/get-track-link', 'id' => $model->id], ['class' => 'btn btn-secondary']);
-                },
-                'remove'  =>  function($model){
-                    return Html::button(FA::i('trash'), ['class' => 'btn btn-secondary removeTrack']);
-                }
-            ],
-            'template'  =>  Html::tag('div', Html::tag('div', '{play}{download}', ['class' => 'btn-group btn-group-sm pull-left']).Html::tag('div', '{remove}{edit}', ['class' => 'btn-group btn-group-sm pull-right']), ['style' => 'width: 150px'])
-        ]
-    ]
-])?>
+                        return $minutes.':'.$seconds;
+                    }
+                ],
+                [
+                    'label'     =>  \Yii::t('manage', 'Добавил'),
+                    'format'    =>  'html',
+                    'attribute' =>  'user_id',
+                    'value'     =>  function($model, $value) use ($searchModel){
+                        if(!$model->user){
+                            return null;
+                        }
+                        /**
+                         * @var $model \common\models\Song
+                         */
+                        return Html::a($model->user->username ? : $model->user->first_name.' '.$model->user->last_name, Url::current([$searchModel->formName() => ['userId' => $model->user_id]]));
+                    }
+                ],
+                [
+                    'label'     =>  \Yii::t('manage', 'Добавлено'),
+                    'attribute' =>  'added',
+                    'format'    =>  'html',
+                    'contentOptions'    =>  [
+                        'class' =>  'text-center'
+                    ],
+                    'value'     =>  function($model){
+                        return \Yii::$app->formatter->asDatetime($model->added, 'MM/dd/yyyy').'&nbsp;'.Html::tag('br', null, ['class' => 'hidden-md hidden-lg']).\Yii::$app->formatter->asDatetime($model->added, 'HH:mm');
+                    }
+                ],
+                [
+                    'class'     =>  \yii\grid\ActionColumn::className(),
+                    'buttons'   =>  [
+                        'edit'  =>  function($url, $model){
+                            return Html::a(FA::i('pencil'), Url::to(['track/edit', 'id' => $model->id]), ['class' => 'btn btn-secondary']);
+                        },
+                        'play'  =>  function($model){
+                            return Html::button(FA::i('play'), ['class' => 'btn btn-secondary pull-left']);
+                        },
+                        'download'  =>  function($url, $model){
+                            return Html::a(FA::i('save'), ['/site/get-track-link', 'id' => $model->id], ['class' => 'btn btn-secondary']);
+                        },
+                        'remove'  =>  function($model){
+                            return Html::button(FA::i('trash'), ['class' => 'btn btn-secondary removeTrack']);
+                        }
+                    ],
+                    'template'  =>  Html::tag('div', Html::tag('div', '{play}{download}', ['class' => 'btn-group btn-group-sm pull-left']).Html::tag('div', '{remove}{edit}', ['class' => 'btn-group btn-group-sm pull-right']), ['style' => 'width: 120px'])
+                ]
+            ]
+        ])?>
+    </div>
+</div>
