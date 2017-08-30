9e6e757c9ab644369f5220fef716aac0b78b66d9
BoBRoID <n.gilko@gmail.com>
2017-08-30 17:35:52 +0300

    fix

--- tg/bot/Actions/ManageNotifications.php
+++ tg/bot/Actions/ManageNotifications.php
@@ -10,14 +10,14 @@ namespace tg\bot\Actions;
 
 
 use app\bot\Entities\InlineKeyboardList;
+use common\helpers\Emoji;
 use common\models\NotificationSettings;
+use common\models\User;
 use Longman\TelegramBot\Entities\InlineKeyboardButton;
 use Longman\TelegramBot\Request;
 
 class ManageNotifications extends BaseAction
 {
-
-
 
     public function run(){
         if(isset($this->queryData->t)){
@@ -30,7 +30,7 @@ class ManageNotifications extends BaseAction
     public function runIndex(){
         $data = [
             'text'          =>  'Настройки уведомлений',
-            'reply_markup'  =>  self::getMainKeyboard()
+            'reply_markup'  =>  $this->getMainKeyboard()
         ];
 
         if($this->update->getCallbackQuery()){
@@ -42,14 +42,19 @@ class ManageNotifications extends BaseAction
         ]);
     }
 
-    public static function getMainKeyboard(){
+    /**
+     * @return InlineKeyboardList
+     * @throws \Longman\TelegramBot\Exception\TelegramException
+     */
+    public function getMainKeyboard(): InlineKeyboardList
+    {
         $buttons = [];
 
         foreach(NotificationSettings::getTypesDescriptions() as $type => $description){
             $buttons[] = new InlineKeyboardButton([
-                'text'  =>  \Yii::t('general', 'Уведомлять, когда {reason} {state}', [
+                'text'  =>  \Yii::t('general', '{reason} {state}', [
                     'reason'        =>  $description,
-                    'state'         =>  ''
+                    'state'         =>  $this->botUser->getNotificationSettingValue($type) ? Emoji::render('bell') : Emoji::render('bell_cancel')
                 ]),
                 'callback_data' =>  json_encode(['action' => 'manageNotifications', 'data' => ['t' => $type]])
             ]);
