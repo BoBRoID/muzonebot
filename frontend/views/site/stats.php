f0c6020bd5e32fa55b3ba86c632bb921d67da1b8
BoBRoID <n.gilko@gmail.com>
2017-09-01 11:56:25 +0300

    layout fix

--- frontend/views/site/stats.php
+++ frontend/views/site/stats.php
@@ -16,23 +16,25 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <h1><?=$this->title?></h1>
 <div class="row">
-    <div class="col-12 col-md-9 offset-md-2">
+    <div class="col-12">
         <h2 class="text-center"><?=\Yii::t('site', 'Топ пользователей')?></h2>
     </div>
-    <div class="col-12 col-md-3">
-        <ul class="list-group">
-            <li class="list-group-item list-group-item-info">
-                <h5 class="list-group-item-heading"><?=\Yii::t('site', 'Кто пользуется ботом?')?></h5>
-            </li>
-            <?php foreach($chatsStats as $statArray){ ?>
-            <li class="list-group-item justify-content-between">
-                <?=$statArray['type']?>
-                <span class="badge badge-default badge-pill"><?=$statArray['count']?></span>
-            </li>
-            <?php } ?>
-        </ul>
+    <div class="col-12">
+        <div class="row">
+            <div class="col-12 col-md-3">
+                <ul class="list-group">
+                    <li class="list-group-item list-group-item-info">
+                        <h5 class="list-group-item-heading"><?=\Yii::t('site', 'Кто пользуется ботом?')?></h5>
+                    </li>
+                    <?php foreach($chatsStats as $statArray){ ?>
+                        <li class="list-group-item justify-content-between">
+                            <?=$statArray['type']?>
+                            <span class="badge badge-default badge-pill"><?=$statArray['count']?></span>
+                        </li>
+                    <?php } ?>
+                </ul>
 
-        <!-- <ul class="list-group">
+                <!-- <ul class="list-group">
             <li class="list-group-item list-group-item-info">
                 <h4 class="list-group-item-heading"><?=\Yii::t('site', 'Сколько в нём треков?')?></h4>
             </li>
@@ -43,57 +45,59 @@ $this->params['breadcrumbs'][] = $this->title;
             </li>
             <?php } ?>
         </ul> -->
-    </div>
-    <div class="col-12 col-md-9">
-        <?php
-        $labels = [];
-        $dataset = new stdClass();
-        $dataset->backgroundColor = \Colors\RandomColor::many(count($topUploaderStats), [
-            'hue'           =>  'purple yellow',
-            'luminosity'    =>  'light',
-        ]);
+            </div>
+            <div class="col-12 col-md-9">
+                <?php
+                $labels = [];
+                $dataset = new stdClass();
+                $dataset->backgroundColor = \Colors\RandomColor::many(count($topUploaderStats), [
+                    'hue'           =>  'purple yellow',
+                    'luminosity'    =>  'light',
+                ]);
 
-        foreach($topUploaderStats as $stat){
-            $dataset->data[] = $stat['count'];
-            $labels[] = $stat['username'] ? : $stat['first_name'].' '.$stat['last_name'];
-        }
+                foreach($topUploaderStats as $stat){
+                    $dataset->data[] = $stat['count'];
+                    $labels[] = $stat['username'] ? : $stat['first_name'].' '.$stat['last_name'];
+                }
 
-        echo ChartJs::widget([
-            'type'  =>  'pie',
-            'clientOptions'   =>  [
-                'legend'=>  [
-                    'position'  =>  'right'
-                ],
-            ],
-            'options'   =>  [
-                'class' =>  'hidden-sm-down'
-            ],
-            'data'  =>  [
-                'datasets'  =>  [
-                    $dataset
-                ],
-                'labels'    =>  $labels
-            ]
-        ]);
+                echo ChartJs::widget([
+                    'type'  =>  'pie',
+                    'clientOptions'   =>  [
+                        'legend'=>  [
+                            'position'  =>  'right'
+                        ],
+                    ],
+                    'options'   =>  [
+                        'class' =>  'd-block d-md-none'
+                    ],
+                    'data'  =>  [
+                        'datasets'  =>  [
+                            $dataset
+                        ],
+                        'labels'    =>  $labels
+                    ]
+                ]);
 
-        echo ChartJs::widget([
-            'type'  =>  'pie',
-            'clientOptions'   =>  [
-                'legend'=>  [
-                    'position'  =>  'bottom'
-                ],
-            ],
-            'options'   =>  [
-                'class' =>  'hidden-md-up',
-                'height'    =>  '400px'
-            ],
-            'data'  =>  [
-                'datasets'  =>  [
-                    $dataset
-                ],
-                'labels'    =>  $labels
-            ]
-        ]);
-        ?>
+                echo ChartJs::widget([
+                    'type'  =>  'pie',
+                    'clientOptions'   =>  [
+                        'legend'=>  [
+                            'position'  =>  'bottom'
+                        ],
+                    ],
+                    'options'   =>  [
+                        'class' =>  'd-none d-md-block',
+                        'height'    =>  '300px'
+                    ],
+                    'data'  =>  [
+                        'datasets'  =>  [
+                            $dataset
+                        ],
+                        'labels'    =>  $labels
+                    ]
+                ]);
+                ?>
+            </div>
+        </div>
     </div>
 </div>
